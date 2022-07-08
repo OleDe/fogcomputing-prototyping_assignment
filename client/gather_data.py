7ed8ef7637e21b144c537e13f73d3fa78992f17a
Jaunty91 <t.kapp@tu-berlin.de>
2022-07-08 11:48:50 +0200

    fix import

--- client/gather_data.py
+++ client/gather_data.py
@@ -3,26 +3,19 @@
 # Require data from bmp280 not tested yet!
 
 import time, subprocess, json
+try:
+    from bmp280 import BMP280
+    from smbus import SMBus
+except ModuleNotFoundError:
+    print("BMP280 or smbus module not found!")
 
 class Gather_data:
-    def __init__(self):
-        self.sensor=True
+    def __init__(self, sensor=False):
         self.bus=None
         self.bmp280=None
-
-        try:
-            from bmp280 import BMP280
-        except ModuleNotFoundError:
-            print("BMP280 module not found!")
-            self.sensor=False
-
-        if self.sensor:
-            try:
-                from smbus2 import SMBus
-            except ImportError:
-                from smbus import SMBus
+        if sensor:
             self.bus = SMBus(1)
-            self.bmp280 = BMP280(i2c_dev=bus)
+            self.bmp280 = BMP280(i2c_dev=self.bus)
                 
     def __gather_data_from_url(self):
         """
@@ -43,13 +36,13 @@ class Gather_data:
         :return: dict, current time in seconds since epoch, air temperature in degree celsius, air pressure in hPa
         """
         tm = {'time': time.time_ns()}
-        if self.sensor:
+        if self.bmp280:
             tm.update({ 'air_temperature': self.bmp280.get_temperature(),
                         'air_pressure': self.bmp280.get_pressure()})
         else:
             tm.update(self.__gather_data_from_url())
         return tm
 if __name__ == '__main__':
-    g = Gather_data()
+    g = Gather_data(sensor=True)
     data = g.gather()
     print(data)
