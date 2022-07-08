0ea0448bca9e95517ea59d390cddacdc8896284b
Jaunty91 <t.kapp@tu-berlin.de>
2022-07-08 17:52:28 +0200

    build image

--- server/build_server_image.sh
+++ server/build_server_image.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-docker network create server-mongodb --driver bridge
-docker run -d --name server-mongodb-server \
+docker network create app-tier --driver bridge
+docker run -d --name db-server \
     -e MONGO_INITDB_ROOT_USERNAME=serv \
     -e MONGO_INITDB_ROOT_PASSWORD=serv123 \
     -e MONGO_INITDB_DATABASE=serv_db \
-    -p 27018:27017 \
-    --network server-mongodb \
+    -p 27017:27017 \
+    -v ./db:/data/db
+    --network app-tier \
+    --restart unless-stopped \
     mongo:4.4.6
 
-# docker build -f Dockerfile -t server:latest .
-# docker run -d -p 50000:50000 server:latest
+docker build -f Dockerfile -t py-server:latest .
+docker run -d --name py-server \
+    -p 50000:50000 \
+    --network app-tier \
+    --restart unless-stopped \
+    --entrypoint "python -u /server.py db-server 27017"
+    py-server:latest
